9aec314996faaa0f37c25f53060ed21e508c19c2
Joaquin Magneres <joaquin.magneres@cymait.com>
2013-07-05 19:32:20 -0300

    Fix attr default value

--- hl7/v3/messages/HL7Message.java
+++ hl7/v3/messages/HL7Message.java
@@ -4,7 +4,7 @@ import javax.xml.bind.annotation.XmlAttribute;
 
 public class HL7Message {
 	@XmlAttribute(name = "ITSVersion", required = true)
-	public String itsVersion;
+	public String itsVersion = "XML_1.0";
 
 	public HL7Message() {
 		super();
